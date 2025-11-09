fe8fe2a98277328b6572a8c550ebf5c0097553b7
Maximiliano Christener <christener@outlook.com.ar>
2025-11-09 15:06:33 -0300

    update: dashboard

--- pre-init.el
+++ pre-init.el
@@ -1,2 +1,12 @@
 ;;; pre-init.el --- DESCRIPTION -*- no-byte-compile: t; lexical-binding: t; -*-
 
+;; size
+;; (add-to-list 'default-frame-alist '(width . 100))  ; Set width in character units
+;; (add-to-list 'default-frame-alist '(height . 40))  ; Set height in line units
+
+(setq default-frame-alist
+             '(
+	           ;; (alpha-background . 92) ;; transparency
+               (width . 100)
+               (height . 40)
+               ))
