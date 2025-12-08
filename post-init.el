3a6c218d6e24284371d5ed3c57625e437d66561e
Maximiliano Christener <christener@outlook.com.ar>
2025-12-08 16:46:22 -0300

    consistent use-package

--- post-init.el
+++ post-init.el
@@ -7,33 +7,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-
-;; markdown-mode (this should be built-in into emacs...)
-;; The markdown-mode package provides a major mode for Emacs for syntax
-;; highlighting, editing commands, and preview support for Markdown documents.
-;; It supports core Markdown syntax as well as extensions like GitHub Flavored
-;; Markdown (GFM).
-(use-package markdown-mode
-  :ensure t
-  :commands (gfm-mode
-             gfm-view-mode
-             markdown-mode
-             markdown-view-mode)
-  :mode (("\\.markdown\\'" . markdown-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("README\\.md\\'" . gfm-mode))
-  :bind
-  (:map markdown-mode-map
-        ("C-c C-e" . markdown-do)))
-
-;; TOC... why not?
-;; Automatically generate a table of contents when editing Markdown files
-(use-package markdown-toc
-  :ensure t
-  :commands (markdown-toc-generate-toc
-             markdown-toc-generate-or-refresh-toc
-             markdown-toc-delete-toc
-             markdown-toc--toc-already-present-p)
-  :custom
-  (markdown-toc-header-toc-title "**Table of Contents**"))
-
--- pre-init.el
+++ pre-init.el
@@ -1,12 +1,10 @@
 ;;; pre-init.el --- DESCRIPTION -*- no-byte-compile: t; lexical-binding: t; -*-
 
-;; size
-;; (add-to-list 'default-frame-alist '(width . 100))  ; Set width in character units
-;; (add-to-list 'default-frame-alist '(height . 40))  ; Set height in line units
-
 (setq default-frame-alist
              '(
-	           ;; (alpha-background . 92) ;; transparency
+               ;; (alpha-background . 92) ;; transparency
                (width . 100)
                (height . 40)
+               (top . 10)
                ))
+
