c312d9f9870a08e08cf62bd490c1c51f987bb5ca
Maximiliano Christener <christener@outlook.com.ar>
2025-12-10 21:27:03 -0300

    del: symlinks behavior

--- post-init.el
+++ post-init.el
@@ -11,3 +11,6 @@
 ;; no me gusta la papelera (esto es de minimal emacs, por default ya es nil)
 (setq delete-by-moving-to-trash nil)
 (setq remote-file-name-inhibit-delete-by-moving-to-trash nil)
+;; lo de los symlinks tampoco me gusta
+(setq find-file-visit-truename nil
+      vc-follow-symlinks 'ask)
